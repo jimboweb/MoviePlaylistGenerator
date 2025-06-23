2d513af6d3b22186ca21d663561b8f7af973c1a4
jimboweb <jimboweb@gmail.com>
2025-06-23 14:24:00 -0400

    read_csv works

--- FileReader.py
+++ FileReader.py
@@ -1,10 +1,13 @@
 from typing import List
 
 def read_csv(filename:str)->List[List[str]]:
-    path = "tmdb5000movies\\"+filename
+    path = "tmdb5000movies/"+filename
     rtrn = []
-    with open(path,'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            rtrn+=line.strip().split(",")
-    return rtrn
+    try:
+        with open(path,'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                rtrn.append(line.strip().split(","))
+        return rtrn
+    except IOError as e:
+        print("exception reading file:" + e)
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# This is a sample Python script.
-
-# Press ⇧F10 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
+from FileReader import read_csv
+def main():
+    movies = read_csv("movies.csv")
+    credits = read_csv("credits.csv")
+    print(movies[0])
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pass
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
